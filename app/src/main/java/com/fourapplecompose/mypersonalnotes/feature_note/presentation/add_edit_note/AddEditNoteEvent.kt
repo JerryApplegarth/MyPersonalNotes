968025321a12aa8c73159e0dd8cbeaf36f50df6f
Jerry Applegarth <applegarth1630@gmail.com>
2021-11-03 10:59:06 -0700

    my tenth commit at 1:57

--- app/src/main/java/com/fourapplecompose/mypersonalnotes/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
+++ app/src/main/java/com/fourapplecompose/mypersonalnotes/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -6,7 +6,10 @@ sealed class AddEditNoteEvent {
 
 
     data class EnteredTitle(val value: String): AddEditNoteEvent()
-    data class ChangeFocusTitle(val focusState: FocusState): AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): AddEditNoteEvent()
     data class EnteredContent(val value: String): AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
+    object SaveNote: AddEditNoteEvent()
 
 }
