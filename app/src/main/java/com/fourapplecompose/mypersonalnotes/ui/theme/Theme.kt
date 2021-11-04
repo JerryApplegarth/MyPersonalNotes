8d420dceb91cb85b58d297362fbdb7040c7c15c0
Jerry Applegarth <applegarth1630@gmail.com>
2021-11-04 06:56:37 -0700

    final commit it is finished with background colors

--- app/src/main/java/com/fourapplecompose/mypersonalnotes/ui/theme/Theme.kt
+++ app/src/main/java/com/fourapplecompose/mypersonalnotes/ui/theme/Theme.kt
@@ -13,9 +13,10 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Green500,
+    primaryVariant = Green700,
+    secondary = Teal200,
+    background = LightYellow
 
     /* Other default colors to override
     background = Color.White,
